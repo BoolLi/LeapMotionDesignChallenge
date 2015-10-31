4e6b937e934ddc5c022b7be74506c335be92fda0
sonmanutd <son.pham@bucknell.edu>
2015-10-31 06:30:06 -0400

    Add color functionality

--- Demo.py
+++ Demo.py
@@ -14,7 +14,8 @@ class Demo(Tkinter.Tk):
         self.Y = 0
         self.Z = 0
         self.hasData = False
-        self.color = "Blues"
+        self.var= "Blues"
+        self.colorVal = "Blues"
 
 
         # GUI
@@ -71,73 +72,11 @@ class Demo(Tkinter.Tk):
 
         colorList = ["Blues", "Greens", "Oranges", "Purples", "Reds", "Greys", "pink"]
 
-        var = Tkinter.StringVar()
-        var.set(colorList[2])
+        self.var = Tkinter.StringVar()
+        self.var.set(colorList[2])
         
-        self.entry6 = Tkinter.OptionMenu(self, var, *colorList)
+        self.entry6 = Tkinter.OptionMenu(self, self.var, *colorList)
         self.entry6.grid(row=2,column=3)
-    
-
-        #self.entry6.pack()
-
-##        self.entry1 = Tkinter.Entry(self)
-##        self.entry2 = Tkinter.Entry(self)
-##        self.entry3 = Tkinter.Entry(self)
-##        self.button1 = Tkinter.Button(self, text="Get Function", command=self.get_function, anchor=W, padx=8)
-##        self.button2 = Tkinter.Button(self, text="Get Color", command=self.get_color,anchor=W, padx=8)
-##        self.button3 = Tkinter.Button(self, text="Get Speed", command=self.get_speed,anchor=W, padx=8)
-##        self.label1 = Tkinter.Label(self, text="Function")
-##        self.label2 = Tkinter.Label(self, text="Color")
-##        self.label3 = Tkinter.Label(self, text="Speed")
-##
-##
-###       self.entry2 = Menubutton
-##        
-##        self.label1.grid(row=0,column=1)
-##        self.button1.grid(row=0,column=3)
-##        self.entry1.grid(row=0,column=2)
-##
-##        self.label2.grid(row=1,column=1)
-##        self.button2.grid(row=1,column=3)
-##        self.entry2.grid(row=1,column=2)
-##        
-##
-##        self.label3.grid(row=2,column=1)
-##        self.button3.grid(row=2,column=3)
-##        self.entry3.grid(row=2,column=2)
-##
-##        self.button4 = Tkinter.Button(self, text="Upper X", command=self.get_upper_x, anchor=W, padx=7)
-##
-##        self.button5 = Tkinter.Button(self, text="Lower X", command=self.get_lower_x, anchor=W, padx=7)
-##        self.button6 = Tkinter.Button(self, text="Upper Y", command=self.get_upper_y, anchor=W, padx=7)
-##        self.button7 = Tkinter.Button(self, text="Lower Y", command=self.get_lower_y, anchor=W, padx=7)
-##        
-##
-##        self.entry4 = Tkinter.Entry(self)
-##        self.entry5 = Tkinter.Entry(self)
-##        self.entry6 = Tkinter.Entry(self)
-##        self.entry7 = Tkinter.Entry(self)
-##
-###       self.label3.grid(row=4,column=1)
-##        self.button4.grid(row=4,column=3)
-##        self.entry4.grid(row=4,column=2)
-##
-###       self.label3.grid(row=2,column=1)
-##        self.button5.grid(row=4,column=5)
-##        self.entry5.grid(row=4,column=4)
-##        
-###       self.label3.grid(row=2,column=1)
-##        self.button6.grid(row=5,column=3)
-##        self.entry6.grid(row=5,column=2)
-##
-###       self.label3.grid(row=2,column=1)
-##        self.button7.grid(row=5,column=5)
-##        self.entry7.grid(row=5,column=4)
-##
-##      self.button8 = Tkinter.Button(self, text="Plot data", command=self.plotData, anchor=W, padx=7)
-##
-
- #       self.button8.grid(row=6,column=5)
 
     def clear(self):
         self.entry1.delete(0,END)
@@ -161,12 +100,13 @@ class Demo(Tkinter.Tk):
     def get_lower_y(self):
         return self.entry3.get()
 
-    def getColor(self):
-        self.color = self.entry6.get()
+##    def getColor(self):
+##        return self.entry6.get()
 
     def plotData(self):
         try:
             infix = self.get_function()
+            infix = infix.replace(" ","")
             endX = float(self.get_upper_x())
             beginX = float(self.get_lower_x())
             endY = float(self.get_upper_y())
@@ -190,13 +130,13 @@ class Demo(Tkinter.Tk):
             self.Y = Y
             self.Z = Z
             self.hasData = True
+            self.colorVal = self.var.get()
 
         except Exception as e:
             self.inputError()
 
     def inputError(self):
         print "Input Error!"
-
     
     def hasNewData(self):
         return self.hasData
@@ -205,4 +145,7 @@ class Demo(Tkinter.Tk):
         self.hasData = False
 
     def getData(self):
-        return self.X, self.Y, self.Z, self.color
+        return self.X, self.Y, self.Z, self.colorVal
+
+app = Demo()
+app.mainloop()
